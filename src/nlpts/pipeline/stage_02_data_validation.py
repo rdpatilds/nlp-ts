2280523a3afc5b646f48973760a5a4e810b8461b
rdpatilds <rdpatilds@gmail.com>
2023-07-22 12:44:54 +0530

    data validation

---
+++ src/nlpts/pipeline/stage_02_data_validation.py
@@ -0,0 +1,13 @@
+from nlpts.config.configuration import ConfigurationManager
+from nlpts.components.data_validation import DataValiadtion
+from nlpts.logging import logger
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_files_exist()
